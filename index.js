693c841ac577b8468f40eee5d3860a5657e86261
Olesya Krayanskaya <krayanskaya_ov@mail.ru>
2025-02-21 15:58:50 +0300

    доработан проект лупа для фото

--- index.js
+++ index.js
@@ -4,54 +4,65 @@ function imageZoom(image, details) {
     let img, lens, result, cx, cy;
     img = document.querySelector(image);
     result = document.querySelector(details);
-    /* Create lens: */
+    /* Создаем лупу */
     lens = document.createElement('DIV');
     lens.setAttribute('class', 'card__img-zoom-lens');
-    /* Insert lens: */
+    /* Добавляем лупу в разметку */
     img.parentElement.insertBefore(lens, img);
-    /* Calculate the ratio between result DIV and lens: */
+    /* Увеличиваем во сколько раз */
     cx = result.offsetWidth / lens.offsetWidth;
     cy = result.offsetHeight / lens.offsetHeight;
-    /* Set background properties for the result DIV */
-    result.style.backgroundImage = `url('' + img.src + '')`;
-    result.style.backgroundSize = (img.width * cx) + 'px ' + (img.height * cy) + 'px';
-    /* Execute a function when someone moves the cursor over the image, or the lens: */
-    lens.addEventListener('mousemove', moveLens);
+    /* Выполняем функцию, когда кто-то перемещает курсор по изображению или лупе */
+    img.addEventListener('mouseenter', startZoom);
     img.addEventListener('mousemove', moveLens);
-    /* And also for touch screens: */
-    lens.addEventListener('touchmove', moveLens);
+    img.addEventListener('mouseleave', resetResult);
+    /* Аналогично, для тач-устройств */
+    img.addEventListener('touchstart', startZoom);
     img.addEventListener('touchmove', moveLens);
+    img.addEventListener('touchend', resetResult);
     function moveLens(e) {
-        var pos, x, y;
-        /* Prevent any other actions that may occur when moving over the image */
+        /* Устанавливаем свойства фона результирующего блока */
+        result.style.backgroundSize = `${(img.width * cx)}px ${(img.height * cy)}px`;
+        let pos, x, y;
         e.preventDefault();
-        /* Get the cursor's x and y positions: */
+        /* Получим координаты x и y курсора относительно изображения */
         pos = getCursorPos(e);
-        /* Calculate the position of the lens: */
+        /* Рассчитываем положение лупы */
         x = pos.x - (lens.offsetWidth / 2);
         y = pos.y - (lens.offsetHeight / 2);
-        /* Prevent the lens from being positioned outside the image: */
-        if (x > img.width - lens.offsetWidth) { x = img.width - lens.offsetWidth; }
+        /* Не пускаем лупу за пределами изображения */
+        if (x > img.offsetWidth - lens.offsetWidth) { x = img.offsetWidth - lens.offsetWidth; }
         if (x < 0) { x = 0; }
-        if (y > img.height - lens.offsetHeight) { y = img.height - lens.offsetHeight; }
+        if (y > img.offsetHeight - lens.offsetHeight) { y = img.offsetHeight - lens.offsetHeight; }
         if (y < 0) { y = 0; }
-        /* Set the position of the lens: */
+        /* Устанавливаем положение лупы */
         lens.style.left = x + 'px';
         lens.style.top = y + 'px';
-        /* Display what the lens 'sees': */
-        result.style.backgroundPosition = '-' + (x * cx) + 'px -' + (y * cy) + 'px';
+        /* Выводим, что под лупой */
+        result.style.backgroundPosition = `-${(x * cx)}px -${(y * cy)}px`;
     }
     function getCursorPos(e) {
-        var a, x = 0, y = 0;
-        /* Get the x and y positions of the image: */
-        a = img.getBoundingClientRect();
-        /* Calculate the cursor's x and y coordinates, relative to the image: */
+        let x = 0, y = 0;
+        /* Получаем координаты x и y изображения */
+        let a = img.getBoundingClientRect();
+        /* Рассчитываем координаты x и y курсора относительно изображения */
         x = e.pageX - a.left;
         y = e.pageY - a.top;
-        /* Consider any page scrolling: */
+        /* Учитываем прокрутку страницы: */
         x = x - window.scrollX;
         y = y - window.scrollY;
         return { x: x, y: y };
+    }
+    const text = document.querySelector('.details__text');
+    function startZoom() {
+        text.classList.add('hidden');
+        result.style.backgroundImage = `url(${img.src})`;
+        lens.classList.remove('hidden');
+    }
+    function resetResult() {
+        text.classList.remove('hidden');
+        result.style.backgroundImage = 'none';
+        lens.classList.add('hidden');
     }
 }
 
